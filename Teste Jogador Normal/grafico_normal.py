844ce05ee9317a658e1e34002783880ae702f558
Rodrigo Navarro <31942397+kappazor@users.noreply.github.com>
2023-06-26 14:25:33 -0300

    Update grafico_normal.py
    versão com barras azuis e verdes e intervalo de confiança

--- Teste Jogador Normal/grafico_normal.py
+++ Teste Jogador Normal/grafico_normal.py
@@ -2,38 +2,58 @@
 # Departamento de Ciência da Computação
 # TCC
 # Rodrigo da Silva Navarro - 15/0147376
-# Código do Modelo Padrão de um Jogo Gacha
+# Código do Modelo Básico de um Jogo Gacha
 #
 #
 #
 import matplotlib.pyplot as plt
 import numpy as np
 
-from padrao5_normal import padrao5normal
-from proposto5_normal import proposto5normal
-from padrao10_normal import padrao10normal
-from proposto10_normal import proposto10normal
-from padrao25_normal import padrao25normal
-from proposto25_normal import proposto25normal
-from padrao50_normal import padrao50normal
-from proposto50_normal import proposto50normal
+# import dos arquivos.py do projeto
+import basico5_f2p as b5f2p
+import completo5_f2p as c5f2p
+import basico10_f2p as b10f2p
+import completo10_f2p as c10f2p
+import basico25_f2p as b25f2p
+import completo25_f2p as c25f2p
+import basico50_f2p as b50f2p
+import completo50_f2p as c50f2p
+
+# chamada das funções que geram a lista para cada nível, do Modelo Básico e do Completo
+b5f2p.basico5f2p_lista()
+c5f2p.completo5f2p_lista()
+b10f2p.basico10f2p_lista()
+c10f2p.completo10f2p_lista()
+b25f2p.basico25f2p_lista()
+c25f2p.completo25f2p_lista()
+b50f2p.basico50f2p_lista()
+c50f2p.completo50f2p_lista()
 
 
 def graph():
+    # largura da barra
     w = 0.3
+    # quantidade de niveis para cada conjunto de barras
     x = ["5", "10", "25", "50"]
-    padrão = [padrao5normal(), padrao10normal(), padrao25normal(), padrao50normal()]
-    proposto = [proposto5normal(), proposto10normal(), proposto25normal(), proposto50normal()]
+
+    # valores das médias
+    básico = [b5f2p.basico5f2p_media(), b10f2p.basico10f2p_media(), b25f2p.basico25f2p_media(), b50f2p.basico50f2p_media()]
+    completo = [c5f2p.completo5f2p_media(), c10f2p.completo10f2p_media(), c25f2p.completo25f2p_media(), c50f2p.completo50f2p_media()]
+
+    # intervalo de confiança para 99% de confiabilidade
+    yer1 = [b5f2p.basico5f2p_erro(), b10f2p.basico10f2p_erro(), b25f2p.basico25f2p_erro(), b50f2p.basico50f2p_erro()]
+    yer2 = [c5f2p.completo5f2p_erro(), c10f2p.completo10f2p_erro(), c25f2p.completo25f2p_erro(), c50f2p.completo50f2p_erro()]
 
     bar1 = np.arange(len(x))
     bar2 = [i + w for i in bar1]
 
-    plt.bar(bar1, padrão, w, label="Modelo Padrão")
-    plt.bar(bar2, proposto, w, label="Modelo Proposto")
+    # configurações das barras
+    plt.bar(bar1, básico, w, color='green', edgecolor='black', yerr=yer1, capsize=3, label="Modelo Básico")
+    plt.bar(bar2, completo, w, color='blue', edgecolor='black', yerr=yer2, capsize=3, label="Modelo Completo")
 
     plt.xlabel("Qtd Total de Níveis")
     plt.ylabel("Game Over")
-    plt.title("Jogadores Normais")
+    plt.title("Jogador Normal")
     plt.xticks(bar1 + w / 2, x)
     plt.legend()
     plt.show()
