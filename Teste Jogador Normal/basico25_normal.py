41a622c2ffc00359c63ca7ac5d376cbba524c351
Rodrigo Navarro <31942397+kappazor@users.noreply.github.com>
2023-07-09 14:22:34 -0300

    Update basico25_normal.py

--- Teste Jogador Normal/basico25_normal.py
+++ Teste Jogador Normal/basico25_normal.py
@@ -2,7 +2,7 @@
 # Departamento de Ciência da Computação
 # TCC
 # Rodrigo da Silva Navarro - 15/0147376
-# Código do Modelo Padrão de um Jogo Gacha
+# Código do Modelo Base de um Jogo Gacha
 #
 #
 #
